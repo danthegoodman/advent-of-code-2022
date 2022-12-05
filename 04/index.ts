b9da95ce0a4a797fe0b73a66dd9fdf53ac07e1a0
Danny Kirchmeier <danny@kirchmeier.us>
2022-12-05 13:57:37 -0600

    day 04

---
+++ 04/index.ts
@@ -0,0 +1,50 @@
+import {readInput} from "../util/read-input.js";
+import * as assert from 'assert';
+
+function main() {
+  const input = readInput(import.meta.url);
+  const example = `\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
+
+  assert.strictEqual(2, solveA(example));
+  assert.strictEqual(507, solveA(input));
+
+  assert.strictEqual(4, solveB(example));
+  assert.strictEqual(897, solveB(input));
+}
+
+main();
+
+function solveA(input: string) {
+  const data = input.split('\n').map(it=>it.match(/\d+/g)!.map(Number));
+  let count = 0;
+  for(const [a,b,x,y] of data){
+    if(a <= x && y <= b){
+      count += 1
+    } else if(x <= a && b <= y){
+      count += 1
+    }
+  }
+  return count;
+}
+function solveB(input: string) {
+  const data = input.split('\n').map(it=>it.match(/\d+/g)!.map(Number));
+  let count = 0;
+  for(const [a,b,x,y] of data){
+    if(a <= x && x <= b){
+      count += 1
+    } else if(a <= y && y <= b){
+      count += 1
+    } else if(x <= a && a <= y){
+      count += 1
+    } else if(x <= b && b <= y){
+      count += 1
+    }
+  }
+  return count;
+}
