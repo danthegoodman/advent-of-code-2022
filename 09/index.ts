500d505ea724689b85b6201677a2e3b764668410
Danny Kirchmeier <danny@kirchmeier.us>
2022-12-09 10:07:10 -0600

    day 09 - part two, initial

--- 09/index.ts
+++ 09/index.ts
@@ -1,20 +1,10 @@
 import {aocTest} from "../util/aoc-test.js";
 
-const example = `\
-R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2
-`;
-
 await aocTest(
   import.meta.url,
   {solveA, solveB},
-  [example, 13, null],
+  [`R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2`, 13, 1],
+  ["R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20", null, 36],
 )
 
 function solveA(input: string) {
@@ -44,5 +34,32 @@ function solveA(input: string) {
 }
 
 function solveB(input: string) {
-  return input.length;
+  const cmds = input.split('\n').map(it=> {
+    const [c, n] = it.split(' ');
+    return c.repeat(Number(n)).split('');
+  }).flat();
+
+  const visited = new Set<string>(['0,0']);
+  const segments = Array.from({length: 10}, ()=> [0,0]);
+  for(const ch of cmds){
+    switch(ch){
+      case 'R': segments[0][1]++; break;
+      case 'L': segments[0][1]--; break;
+      case 'U': segments[0][0]++; break;
+      case 'D': segments[0][0]--; break;
+    }
+
+    for(let i = 1; i < segments.length;i++){
+      const curr = segments[i];
+      const prev = segments[i-1];
+      const dr = prev[0] - curr[0];
+      const dc = prev[1] - curr[1];
+      if(Math.abs(dr) > 1 || Math.abs(dc) > 1){
+        curr[0] += dr === 0 ? 0 : (dr < 0 ? -1 : 1);
+        curr[1] += dc === 0 ? 0 : (dc < 0 ? -1 : 1);
+      }
+    }
+    visited.add(segments.at(-1)!.join(','))
+  }
+  return visited.size;
 }
