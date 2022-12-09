57419643721b211bf85e695f312f14a8ab4d5e4a
Danny Kirchmeier <danny@kirchmeier.us>
2022-12-09 10:09:19 -0600

    day 09 - simplified

--- 09/index.ts
+++ 09/index.ts
@@ -8,39 +8,21 @@ await aocTest(
 )
 
 function solveA(input: string) {
-  const cmds = input.split('\n').map(it=> {
-    const [c, n] = it.split(' ');
-    return c.repeat(Number(n)).split('');
-  }).flat();
-
-  const visited = new Set<string>(['0,0']);
-  let hr=0,hc=0,tr=0,tc=0;
-  for(const ch of cmds){
-    switch(ch){
-      case 'R': hc++; break;
-      case 'L': hc--; break;
-      case 'U': hr++; break;
-      case 'D': hr--; break;
-    }
-
-    const dr = hr - tr;
-    const dc = hc - tc;
-    if(Math.abs(dr) <= 1 && Math.abs(dc) <= 1) continue;
-    tr += dr === 0 ? 0 : (dr < 0 ? -1 : 1);
-    tc += dc === 0 ? 0 : (dc < 0 ? -1 : 1);
-    visited.add(`${tr},${tc}`)
-  }
-  return visited.size;
+  return solveCommon(input, 2);
 }
 
 function solveB(input: string) {
+  return solveCommon(input, 10);
+}
+
+function solveCommon(input: string, knots: number){
   const cmds = input.split('\n').map(it=> {
     const [c, n] = it.split(' ');
     return c.repeat(Number(n)).split('');
   }).flat();
 
   const visited = new Set<string>(['0,0']);
-  const segments = Array.from({length: 10}, ()=> [0,0]);
+  const segments = Array.from({length: knots}, ()=> [0,0]);
   for(const ch of cmds){
     switch(ch){
       case 'R': segments[0][1]++; break;
