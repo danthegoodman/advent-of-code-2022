e39f32af01e113915044ddcf2feda8337c91b8df
Danny Kirchmeier <danny@kirchmeier.us>
2022-12-11 23:47:48 -0600

    day 12 - cleaned up

--- 12/index.ts
+++ 12/index.ts
@@ -17,81 +17,59 @@ await aocTest(
 )
 
 function solveA(input: string) {
-  const grid = input.split('\n').map(it=>[...it]);
-  const count = grid.map(r=> r.map(()=> NaN));
-  let posQueue: [number,number][] = [];
-  for(let r = 0; r < grid.length; r++){
-    for(let c = 0; c < grid[r].length; c++){
-      if(grid[r][c] === 'S'){
-        posQueue.push([r,c])
-        count[r][c] = 0;
-      }
-    }
-  }
-
-  function tryVisit(currCh: string, currCount: number, r: number, c:number){
-    const nextCh = grid[r]?.[c];
-    if(nextCh === undefined) return;
-    if(Number.isFinite(count[r][c])) return;
-
-    const currLvl = currCh === 'S' ? 0 : currCh.charCodeAt(0) - A_CODE;
-    const nextLvl = nextCh === 'E' ? 26 : nextCh.charCodeAt(0) - A_CODE;
-    if(nextLvl - 1 > currLvl) return;
-    // console.log(currCh, '->', nextCh)
-    count[r][c] = currCount + 1;
-    posQueue.push([r,c]);
-  }
-  while(posQueue.length){
-    const [r,c] = posQueue.shift()!;
-    const curr = grid[r][c];
-    const currCount = count[r][c];
-    if(curr === 'E') {
-      return currCount;
-    }
-    tryVisit(curr,currCount, r-1, c);
-    tryVisit(curr,currCount, r+1, c);
-    tryVisit(curr,currCount, r, c-1);
-    tryVisit(curr,currCount, r, c+1);
-  }
-  return input && null;
+  return solveCommon(input, 'up')
+}
+function solveB(input: string) {
+  return solveCommon(input, 'down')
 }
 
-function solveB(input: string) {
-  const grid = input.split('\n').map(it=>[...it]);
-  const count = grid.map(r=> r.map(()=> NaN));
-
-  let posQueue: [number,number][] = [];
+function findPos(grid: string[][], char: string){
   for(let r = 0; r < grid.length; r++){
     for(let c = 0; c < grid[r].length; c++){
-      if(grid[r][c] === 'E'){
-        posQueue.push([r,c])
-        count[r][c] = 0;
+      if(grid[r][c] === char){
+        return [r,c] as const;
       }
     }
   }
-
-  function tryVisit(currCh: string, currCount: number, r: number, c:number){
-    const nextCh = grid[r]?.[c];
-    if(nextCh === undefined) return;
-    if(Number.isFinite(count[r][c])) return;
-
-    const currLvl = currCh === 'E' ? 26 : currCh.charCodeAt(0) - A_CODE;
-    const nextLvl = nextCh === 'S' ? 0 : nextCh.charCodeAt(0) - A_CODE;
-    console.log(currCh, nextCh)
-    if(nextLvl < currLvl - 1) return;
-    count[r][c] = currCount + 1;
-    posQueue.push([r,c]);
+  throw new Error("not found");
+}
+function getCode(char: string){
+  switch (char){
+    case 'S': return 0;
+    case 'E': return 26;
   }
+  return char.charCodeAt(0) - A_CODE;
+}
+
+function solveCommon(input: string, going: 'up' | 'down'){
+  const startChar = going === 'up' ? 'S' : 'E';
+  const endChar = going === 'up' ? 'E' : 'a';
+  const grid = input.split('\n').map(it=>[...it]);
+  const count = grid.map(r=> r.map(()=> -1));
+  const start = findPos(grid, startChar);
+  const posQueue = [ start ];
+  count[start[0]][start[1]] = 0;
+
   while(posQueue.length){
     const [r,c] = posQueue.shift()!;
-    const curr = grid[r][c];
     const currCount = count[r][c];
-    if(curr === 'a') {
-      return currCount;
+    const currLvl = getCode(grid[r][c]);
+    const consider = [[r-1,c],[r+1,c],[r,c-1],[r,c+1]] as const;
+
+    for(const [r2,c2] of consider){
+      if(count[r2]?.[c2] !== -1) continue;
+      const nextCh = grid[r2][c2];
+      const nextLvl = getCode(nextCh);
+      if(going === 'up'){
+        if(nextLvl - 1 > currLvl) continue;
+      } else {
+        if(nextLvl + 1 < currLvl) continue;
+      }
+
+      if(nextCh === endChar) return currCount + 1;
+      count[r2][c2] = currCount + 1;
+      posQueue.push([r2,c2]);
     }
-    tryVisit(curr,currCount, r-1, c);
-    tryVisit(curr,currCount, r+1, c);
-    tryVisit(curr,currCount, r, c-1);
-    tryVisit(curr,currCount, r, c+1);
   }
+  throw new Error("never found")
 }
